73e4f06e0f242e22ebe634347cad2ae135240fd9
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-09-30 06:34:29 -0700

    feat: complete set up page

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Navigator from './src/navigation';
+import {Provider} from 'react-redux';
+import store from './src/store';
 
 export default function App() {
-  return <Navigator />;
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
 }
