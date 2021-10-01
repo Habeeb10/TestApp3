0b9bc88de657073e0fd1b26a30d14421143a18e3
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-10-01 12:55:29 -0700

    chore: change files type

--- assets/index.js
+++
@@ -1,1 +0,0 @@
-export const AuthorPix = require('./authorPix.png');
--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import {init} from '@rematch/core';
-import {news_Store} from './models/models';
-
-const store = init({models: {news_Store}});
-
-export default store;
