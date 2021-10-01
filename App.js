e795cfc539887f04d23c55b918cb807e96c6dffa
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-10-01 12:57:33 -0700

    chore: change file path

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import Navigator from './src/navigation';
 import {Provider} from 'react-redux';
-import store from './src/store';
+import {store} from './src/store';
 
 export default function App() {
   return (
