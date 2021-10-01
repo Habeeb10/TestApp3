8240e0248d2dae465af1a56cbf05013061fb4c02
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-10-01 12:57:58 -0700

    chore: set up configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,33 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['react', 'react-native', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 0,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.jsx'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': ['off'],
+        '@typescript-eslint/explicit-function-return-type': ['error'],
+      },
+    },
+  ],
 };
